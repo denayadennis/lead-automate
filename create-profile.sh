1ecc20daf8598508969297431b6365d83b0bab90
Dut <dutandrew78@gmail.com>
2023-05-07 16:16:20 +0300

    add trim functionaality]

--- create-profile.sh
+++ create-profile.sh
@@ -10,7 +10,6 @@ read F_NAME
 # removing extra unnecessary spaces in the string.
 F_NAME=$(echo "$F_NAME" | tr -s ' ')
 
-
 # name separeted by dashes to be create image and markdown files 'Dut-Andrew-Kulang/' 'Dut-Andrew-Kulang.md'
 D_NAME="${F_NAME//' '/'-'}"
 
@@ -23,27 +22,56 @@ path_to_profile=$profiles_home_path$D_NAME.md
 if [ ! -f "$path_to_profile" ]
     then
     read -p $'Hub/organization\n' HUB
+    HUB=$(echo "$HUB" | tr -s ' ')
 
     read -p $'In which country are you active in?\n' COUNTRY
+    COUNTRY=$(echo "$COUNTRY" | tr -s ' ')
+
 
     read -p $"In region/district/state of $COUNTRY are you operatin in?\n" REGION
+    REGION=$(echo "$REGION" | tr -s ' ')
 
     read -p $'E-mail\n' EMAIL
+    EMAIL=$(echo "$EMAIL" | tr -s ' ')
 
 
     read -p $'Your phone number e.g. +256782....\n' PHONE_NUMBER
+    PHONE_NUMBER=$(echo "$PHONE_NUMBER" | tr -s ' ')
+    
     read -p $'Whatsapp Number e.g +2119..\n' WHATSAPP_NUMBER
-    read -p $'Website URL e.g https://lead.asknet.community\n' WEBSITE_URL
-    read -p $'Telegram username e.g UserName\n' TELEGRAM
-    read -p $'GitHub Username e.g UserName\n' GITHUB
-    read -p $'URL to LinkedIn profile\n' LINKEDIN
-    read -p $'Your twitter username\n' TWITTER
-    read -p $'Your Facebook username\n' FACEBOOK
-    read -p $'Your instagram username\n' INSTAGRAM
-    read -p $'Mastodon profile link\n' MASTODON
-    read -p $'Your wikifab username\n' WIKIFAB
-    read -p $'Other\n' OTHER
+    WHATSAPP_NUMBER=$(echo "$WHATSAPP_NUMBER" | tr -s ' ')
 
+    read -p $'Website URL e.g https://lead.asknet.community\n' WEBSITE_URL
+
+    read -p $'Telegram username e.g UserName\n' TELEGRAM
+    TELEGRAM=$(echo "$TELEGRAM" | tr -s ' ')
+
+    read -p $'GitHub Username e.g UserName\n' GITHUB
+    GITHUB=$(echo "$GITHUB" | tr -s ' ')
+
+    read -p $'URL to LinkedIn profile\n' LINKEDIN
+    LINKEDIN=$(echo "$LINKEDIN" | tr -s ' ')
+
+    read -p $'Your twitter username\n' TWITTER
+    TWITTER=$(echo "$TWITTER" | tr -s ' ')
+
+    read -p $'Your Facebook username\n' FACEBOOK
+    FACEBOOK=$(echo "$FACEBOOK" | tr -s ' ')
+
+    read -p $'Your instagram username\n' INSTAGRAM
+    INSTAGRAM=$(echo "$INSTAGRAM" | tr -s ' ')
+
+    read -p $'Mastodon profile link\n' MASTODON
+    MASTODON=$(echo "$MASTODON" | tr -s ' ')
+
+    read -p $'Your wikifab username\n' WIKIFAB
+    WIKIFAB=$(echo "$WIKIFAB" | tr -s ' ')
+
+    read -p $'Other\n' OTHER
+    OTHER=$(echo "$OTHER" | tr -s ' ')
+
+    read -p $'Profile bio' BIO
+    BIO=$(echo "$BIO" | tr -s ' ')
 
     #check if the expert already has an image directory
 
@@ -62,31 +90,30 @@ if [ ! -f "$path_to_profile" ]
         mkdir -p _profiles    
     fi
 
+echo "---
+layout: profile
+title: \"$F_NAME\"
+image: \"$IMAGELINK$D_NAME/\"
+country: $COUNTRY
+region: $REGION
+hub: $HUB
+languages:
+mail: $EMAIL
+phone: \"$PHONE_NUMBER\"
+whatsapp: \"$WHATSAPP_NUMBER\"
+website: $WEBSITE_URL
+telegram: $TELEGRAM
+github: $GITHUB
+linkedin: $LINKEDIN
+twitter: $TWITTER
+facebook: $FACEBOOK
+instagram: $INSTAGRAM
+mastodon: $MASTODON
+wikifab: $WIKIFAB
 
-    # check if the expert already has a profile .md file 
-
-        
-    echo "---
-    layout: profile
-    title: \"$F_NAME\"
-    image: \"$IMAGELINK$D_NAME/\"
-    country: $COUNTRY
-    region: $REGION
-    hub: $HUB
-    languages:
-    mail: $EMAIL
-    phone: $PHONE_NUMBER
-    whatsapp: $WHATSAPP_NUMBER
-    website: $WEBSITE_URL
-    telegram: $TELEGRAM
-    github: $GITHUB
-    linkedin: $LINKEDIN
-    twitter: $TWITTER
-    facebook: $FACEBOOK
-    instagram: $INSTAGRAM
-    mastodon: $MASTODON
-    wikifab: $WIKIFAB
-    " > $path_to_profile
+---
+$BIO
+" > $path_to_profile
     else
         echo "$F_NAME already has a profile file at $path_to_profile."
 fi
